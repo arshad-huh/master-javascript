1b10b2442f3730a00b9ac13d9780d73839f74b59
arshad-huh <145920436+arshad-huh@users.noreply.github.com>
2024-12-10 01:10:20 +0530

    Update practice-1.js

--- basics-of-javascript/practice-1.js
+++ basics-of-javascript/practice-1.js
@@ -32,13 +32,45 @@ console.log(typeof nullVariable); // "object"
 
 
 // Convert a string "42" into a number and demonstrate it with code.
+let str = '42';
+let num = +str;
+console.log(num);
+// OR 
+num = Number(str);
+console.log(num);
+
+
 // Strings
+
 // Write code to concatenate two strings using both the + operator and template literals.
+let str1 = 'Hello';
+let str2 = 'Arshad';
+
+console.log(str1 + ' ' + str2);
+console.log(`${str1} ${str2}`);
+
 // Create a multiline string using a template literal and include a variable inside it.
+let name = 'Arshad';
+
+let multilineString = `
+Name: ${name}
+Age: 20
+`;
+
+
 // Write a program that takes a string, removes leading and trailing whitespaces using trim(), and converts it to uppercase.
+let strH = '   Hello, World!   ';
+console.log(strH.trim().toUpperCase());
+
 // Operators
+
 // Write a program to demonstrate the difference between == and === with examples.
+console.log(5 == "5");  // Output: true (loose equality allows type coercion)
+console.log(5 === "5"); // Output: false (strict equality checks type and value)
+
 // Use the ternary operator to assign a value to a variable based on whether another variable is greater than 10.
+let a = 5;
+
 // Write a program to calculate the remainder when 25 is divided by 4 using the modulus operator.
 // Arithmetic and Logical Operators
 // Write a program that increments a variable by 1 using both prefix and postfix notation. Print the results.
